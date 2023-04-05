efa15349a63ac6c848069bb8507b07544d2c2d03
Fadel <59105868+SrWither@users.noreply.github.com>
2023-04-05 01:07:24 -0300

    Delete todo.sql

--- src/todo.sql
+++
@@ -1,8 +0,0 @@
-DEFINE TABLE Todo SCHEMALESS;
-
-DEFINE FIELD task ON TABLE Todo TYPE string
-  ASSERT $value != NONE;
-DEFINE FIELD completed ON TABLE Todo TYPE bool
-  ASSERT $value != NONE;
-
-CREATE Todo SET task = 'Prueba', completed: false
